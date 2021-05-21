ceb3e17200608ffe8d87b70149f3f7a9194c33db
Ciro Dourado <>
2021-05-21 16:20:11 -0300

    realocando metodos para Nome

--- src/Pessoa.cs
+++ src/Pessoa.cs
@@ -1,10 +1,13 @@
+using System;
+using System.Collections.Generic;
+
 public class Pessoa {
 	private string _localizacao = string.Empty;
 	public  string Nome  { get; set; }
 	public  int    Idade { get; set; }
 
 	public Pessoa() {
-		Nome  = string.Empty;
+		Nome  = "Sem nome";
 		Idade = 0;
 	} // Pessoa
 
@@ -13,11 +16,53 @@ public class Pessoa {
 		Idade = idade;
 	} // Pessoa
 
+
+    // Metodos
+    public string IniciaisNome() {
+        return String.IsNullOrEmpty(Nome)?
+            string.Empty:
+            _Iniciais(Nome);
+    } // IniciaisNome
+
+
+    private string _Iniciais(string nomeCompleto) {
+        var iniciais = new Queue<char>();
+
+        foreach(string nome in nomeCompleto.Split(' ')) {
+            var inicial = _ObterInicial(nome);
+            iniciais.Enqueue(inicial);
+        }
+        return _IniciaisFormatadas(iniciais);
+    } // _Iniciais
+
+
+    private char _ObterInicial(string nome) {
+        return Convert.ToChar(nome[0]);
+    } // _ObterInicial
+
+
+    private string _IniciaisFormatadas(Queue<char> iniciais) {
+        var primeira = _InicialFormatada(iniciais);
+        var segunda  = _InicialFormatada(iniciais);
+        return $"{primeira}{segunda}";
+    } // _IniciaisFormatadas
+
+
+    private string _InicialFormatada(Queue<char> iniciais) {
+        return iniciais.Count >= 1?
+            Char.ToUpper(iniciais.Dequeue()) + ".":
+            string.Empty;
+    } // _ObterInicial
+
+
+/*	
 	public void Realocar(string localizacao) {
 		if (!string.IsNullOrEmpty(localizacao)) {
 			_localizacao = localizacao;
 		}
 	} // end Realocar
+*/
+
 
 /*
 	public float GetDistancia(string localizacao) {
@@ -25,5 +70,6 @@ public class Pessoa {
 			.GetDistancia(_localizacao, localizacao);
 	} // end GetDistancia
 */
+
 
 } // end class Pessoa
