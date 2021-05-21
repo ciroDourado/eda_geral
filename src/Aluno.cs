0417ae2bff8ae8f13f1d7e8c005f470d5d9305ff
Ciro Dourado <>
2021-05-21 16:19:19 -0300

    refatorando classe

--- src/Aluno.cs
+++ src/Aluno.cs
@@ -1,55 +1,14 @@
 using System;
-using System.Collections.Generic;
 
 public class Aluno: Pessoa {
 	public string Nota { get; set; }
 
 	public Aluno() {
-		Nome = string.Empty;
 		Nota = string.Empty;
 	} // construtor
 
-	public Aluno(string nome, string nota) {
-		Nome = nome;
+	public Aluno(string nota) {
 		Nota = nota;
 	} // construtor
-
-
-	// Metodos
-	public string IniciaisNome() {
-		return String.IsNullOrEmpty(Nome)? 
-			string.Empty:
-			_Iniciais(Nome);
-	} // IniciaisNome
-
-	
-	private string _Iniciais(string nomeCompleto) {
-		var iniciais = new Queue<char>();
-
-		foreach(string nome in nomeCompleto.Split(' ')) {
-			var inicial = _ObterInicial(nome);
-			iniciais.Enqueue(inicial);
-		}
-		return _IniciaisFormatadas(iniciais);
-	} // _Iniciais
-
-
-	private char _ObterInicial(string nome) {
-		return Convert.ToChar(nome[0]);
-	} // _ObterInicial
-
-	
-	private string _IniciaisFormatadas(Queue<char> iniciais) {
-		var primeira = _InicialFormatada(iniciais);
-		var segunda  = _InicialFormatada(iniciais);
-		return $"{primeira}{segunda}";
-	} // _IniciaisFormatadas
-
-	
-	private string _InicialFormatada(Queue<char> iniciais) {
-		return iniciais.Count >= 1?
-			Char.ToUpper(iniciais.Dequeue()) + ".":
-			string.Empty;
-	} // _ObterInicial
 
 } // end class Aluno
