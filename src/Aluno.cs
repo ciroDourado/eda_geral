1933b72db6eebd65463922a41bb9904442882e0d
Ciro Dourado <>
2021-05-22 14:51:19 -0300

    atributo notas e metodos

--- src/Aluno.cs
+++ src/Aluno.cs
@@ -1,25 +1,64 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Aluno: Pessoa {
-	public string Nota { get; set; }
+	public  string      Descricao { get; set; }
+	private List<float> notas;
 
 	public Aluno() {
-		Nota = string.Empty;
+		Descricao = string.Empty;
+		notas     = new List<float>();
 	} // construtor
 
-	public Aluno(string nota) {
-		Nota = nota;
+	public Aluno(string descricao) {
+		Descricao = descricao;
 	} // construtor
 
-	public Aluno(string nota, string nome) {
+	public Aluno(string descricao, string nome) {
+		// classe-pai
 		Nome = nome;
-		Nota = nota;
+		
+		// classe Aluno
+		Descricao = descricao;
 	} // construtor
 
-	public Aluno(string nota, string nome, int idade) {
-		Nota  = nota;
+	public Aluno(string descricao, string nome, int idade) {
+		// classe-pai
 		Nome  = nome;
 		Idade = idade;
+		
+		// classe Aluno
+		Descricao = descricao;
 	} // construtor
+
+
+	public void InserirNota(float nota) {
+		var erro = "Todas as notas foram preenchidas";
+		
+		if( notas.Count <= 3 ) {
+			notas.Add(nota);
+		} else Console.WriteLine(erro);
+	} // InserirNota
+
+
+	public void CorrigirNota(int ordinal, float nota) {
+		var erro = "Fora do intervalo";
+		int indice = ordinal - 1;
+
+		if( _EstaNoIntervalo(indice) ) {
+			notas[indice] = nota;
+		} else Console.WriteLine(erro);
+	} // CorrigirNota
+
+	
+	private bool _EstaNoIntervalo(int indice) {
+		return indice >= 0 && indice < notas.Count;
+	} // _EstaNoIntervalo
+
+
+	public float Media() {
+		return notas.Average();
+	} // Media
 
 } // end class Aluno
